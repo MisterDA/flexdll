61037c13ae0dfe3d99f745c3b714388a102dbe73
Antonin Décimo <antonin@tarides.com>
2025-03-27 14:23:01 +0100

    Reuse the compilers used during the build for later operations
    Record the compiler used during FlexDLL's build in a new module.
    
    In the case of MINGW* and CYGWIN64 toolchains, this allows using
    alternative compilers such as clang. Otherwise, FlexDLL would default
    to querying GCC, which may report different path list than clang.
    
    In the case of MSVC and MSVC64 toolchains, this allows using
    alternative compilers such as clang-cl. Otherwise, FlexDLL would
    default to using cl.exe, which may not be desired or even available.

--- reloc.ml
+++ reloc.ml
@@ -24,7 +24,15 @@ let debug ?(dry_mode = {contents=false}) min_level fmt =
 let search_path = ref []
 let default_libs = ref []
 
-let gcc = ref "gcc"
+let cc = function
+  | `MSVC -> Build_config.msvc
+  | `MSVC64 -> Build_config.msvc64
+  | `CYGWIN64 -> Build_config.cygwin64
+  | `MINGW -> Build_config.mingw
+  | `MINGW64 -> Build_config.mingw64
+  | `GNAT | `GNAT64 -> Build_config.gnat
+  | _ -> failwith "No C compiler configured for this toolchain."
+
 let objdump = ref "objdump"
 
 let is_crt_lib = function
@@ -1173,7 +1181,7 @@ let build_dll link_exe output_file files exts extra_args =
         in
         Printf.sprintf
           "%s %s%s%s -L. %s %s -o %s %s %s %s %s"
-          !gcc
+          (cc !toolchain)
           (Option.fold ~none:"" ~some:(fun ld -> "-fuse-ld=" ^ ld ^ " ") !Cmdline.use_linker)
           (if link_exe = `EXE then "" else "-shared ")
           (if main_pgm then "" else if !noentry then "-Wl,-e0 " else if !machine = `x86 then "-Wl,-e_FlexDLLiniter@12 " else "-Wl,-eFlexDLLiniter ")
@@ -1195,7 +1203,7 @@ let build_dll link_exe output_file files exts extra_args =
         in
         Printf.sprintf
           "%s -m%s %s%s%s -L. %s %s -o %s %s %s %s %s %s"
-          !gcc
+          (cc !toolchain)
           !subsystem
           (Option.fold ~none:"" ~some:(fun ld -> "-fuse-ld=" ^ ld ^ " ") !Cmdline.use_linker)
           (if link_exe = `EXE then "" else "-shared ")
@@ -1338,7 +1346,6 @@ let remove_duplicate_paths paths =
 
 let setup_toolchain () =
   let mingw_libs pre =
-    gcc := pre ^ "gcc";
     objdump := pre ^ "objdump";
     let rec get_lib_search_dirs install libraries input =
       match input with
@@ -1378,13 +1385,13 @@ let setup_toolchain () =
             libraries
     in
     let lib_search_dirs =
-      get_lib_search_dirs "" "" (get_output "%s -print-search-dirs" !gcc)
+      get_lib_search_dirs "" "" (get_output "%s -print-search-dirs" (cc !toolchain))
       |> List.map normalize_path
       |> remove_duplicate_paths
     in
     search_path := !dirs @ lib_search_dirs;
     if !verbose >= 1 then begin
-      print_endline "lib search dirs:";
+      Printf.printf "lib search dirs (%s):" (cc !toolchain);
       List.iter (Printf.printf "  %s\n") lib_search_dirs;
       flush stdout
     end;
@@ -1410,14 +1417,13 @@ let setup_toolchain () =
       add_flexdll_obj := false;
       noentry := true
   | `CYGWIN64 ->
-      gcc := "gcc";
       objdump := "objdump";
       search_path :=
         !dirs @
           [
            "/lib";
            "/lib/w32api";
-           Filename.dirname (get_output1 ~use_bash:true "gcc -print-libgcc-file-name");
+           Filename.dirname (get_output1 ~use_bash:true "%s -print-libgcc-file-name" (cc !toolchain));
           ];
       default_libs := ["-lkernel32"; "-luser32"; "-ladvapi32";
                        "-lshell32"; "-lcygwin"; "-lgcc_s"; "-lgcc"]
@@ -1427,16 +1433,16 @@ let setup_toolchain () =
       if not !custom_crt then
         default_libs := ["msvcrt.lib"]
   | `MINGW ->
-      mingw_libs Version.mingw_prefix
+      mingw_libs Build_config.mingw_prefix
   | `MINGW64 ->
-      mingw_libs Version.mingw64_prefix
+     mingw_libs Build_config.mingw64_prefix
   | `GNAT | `GNAT64 ->
    (* This is a plain copy of the mingw version, but we do not change the
       prefix and use "gnatls" to compute the include dir. *)
     search_path :=
       !dirs @
       [
-       Filename.dirname (get_output1 "%s -print-libgcc-file-name" !gcc);
+       Filename.dirname (get_output1 "%s -print-libgcc-file-name" (cc !toolchain));
        read_gnatls ();
       ];
     default_libs :=
@@ -1475,21 +1481,23 @@ let compile_if_needed file =
     let cmd = match !toolchain with
       | `MSVC | `MSVC64 ->
           Printf.sprintf
-            "cl /c /MD /nologo /Fo%s %s %s%s"
+            "%s /c /MD /nologo /Fo%s %s %s%s"
+            (cc !toolchain)
             (Filename.quote tmp_obj)
             (mk_dirs_opt "/I")
             file
             pipe
       | `CYGWIN64 ->
           Printf.sprintf
-            "gcc -c -o %s %s %s"
+            "%s -c -o %s %s %s"
+            (cc !toolchain)
             (Filename.quote tmp_obj)
             (mk_dirs_opt "-I")
             file
       | `MINGW | `MINGW64 | `GNAT | `GNAT64 ->
           Printf.sprintf
             "%s -c -o %s %s %s"
-            !gcc
+            (cc !toolchain)
             (Filename.quote tmp_obj)
             (mk_dirs_opt "-I")
             (Filename.quote file)
